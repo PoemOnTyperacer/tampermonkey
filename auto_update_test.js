065c6900c778edfb1d14f131bd8555ec50183751
PoemOnTyperacer <63360610+PoemOnTyperacer@users.noreply.github.com>
2020-04-08 17:42:19 +0200

    Create auto_update_test.js

---
+++ auto_update_test.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Test script
+// @description  testing auto-update
+// @namespace    http://tampermonkey.net/
+// @author       newbie
+// @version      1.0
+// @updateURL    https://github.com/mygithubaccount/test/raw/master/test.user.js
+// @downloadURL  https://github.com/mygithubaccount/test/raw/master/test.user.js
+// @match        http://awebsite.com/*
+// @run-at       document-end
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceURL
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
