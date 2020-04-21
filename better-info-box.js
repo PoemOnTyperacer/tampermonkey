36db9f775fa20d3d371b6e50261300d19ec874b7
PoemOnTyperacer <63360610+PoemOnTyperacer@users.noreply.github.com>
2020-04-21 22:40:44 +0200

    Update better-info-box.js

--- better-info-box.js
+++ better-info-box.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Typeracer: Better Info Box Data
 // @namespace    http://tampermonkey.net/
-// @version      0.10.0
+// @version      0.10.1
 // @updateURL    https://raw.githubusercontent.com/PoemOnTyperacer/tampermonkey/master/better-info-box.js
 // @downloadURL  https://raw.githubusercontent.com/PoemOnTyperacer/tampermonkey/master/better-info-box.js
 // @description  Last 10 avg to two decimal places, & proper thousands separator formatting, in the User Info Box
 // @author       poem
 // @match        https://play.typeracer.com/*
+// @match        https://staging.typeracer.com/*
 // @match        https://data.typeracer.com/pit/*
 // @grant        GM_xmlhttpRequest
 // @connect      data.typeracer.com
@@ -21,6 +22,8 @@
                      Proper commas formatting in avg (for 1000+cpm/wpm typists)
 0.9.0  (04-20-20):   Fixed history
 0.10.0 (04-21-20):   Removed refreshing latency (for "normal", single-tab racing)
+0.10.1 (04-21-20):   Staging support
+                     Better log in or out support
 ====================================================================================*/
 
 // Global text boxes variables
@@ -32,11 +35,26 @@ const integerCommasRegex = /\B(?=(\d{3})+(?!\d))/g;
 const displayNameRegex = /.*\((.*)\)$/;
 
 var isRacing=false;
+var playerName='';
 
+// Contextual values
+var context=0; //0: play or staging; 1: pit stop
+const displayNameClass = ['userNameLabel','mainUserName'];
+const dataRowClass = ['datarow','dataRow'];
+const avgAppendix=['',' <a href="http://wikipedia.org/wiki/Wpm" target="_blank">WPM</a>'];
+
+// Detect universe
 var accountDataUrlBase = 'https://data.typeracer.com/users?id=tr:';
 let match = /.*(universe=)([^&]*)/.exec(window.location.href);
 if(match!=null&&match[2]!='')
-    accountDataUrlBase = 'https://data.typeracer.com/users?'+match[1]+match[2]+'&id=tr:';
+	accountDataUrlBase = 'https://data.typeracer.com/users?'+match[1]+match[2]+'&id=tr:';
+
+function checkContext() // Returns 0 for play or staging, 1 for Pit Stop
+{
+    if(document.getElementsByClassName('mainUserInfoBoxWpm').length>0)
+        return 0;
+    return 1;
+}
 
 // Adds proper thousands separators to an int
 function formatInteger(n) {
@@ -72,9 +90,9 @@ function getUsernameFromDisplayName(displayName) {
 }
 
 // Get, format and display exact last 10 average
-function exactAvg(displayNameClass,pitStopAppendix='')
+function exactAvg()
 {
-    let playerName = getUsernameFromDisplayName(document.getElementsByClassName(displayNameClass)[0].innerText); // Check username everytime to support logging in and out without refreshing
+    let playerName = getUsernameFromDisplayName(document.getElementsByClassName(displayNameClass[context])[0].innerText); // Check username everytime to support logging in and out without refreshing
 
 //     If logged out as guest: can't access exact average
     if(playerName=='Guest')
@@ -90,66 +108,17 @@ function exactAvg(displayNameClass,pitStopAppendix='')
 		onload: function (response) {
 			let data = JSON.parse(response.responseText);
 			let current_avg = data.tstats.recentAvgWpm;
-            avgDisplay.innerHTML=formatSpeed(wpmOrCpm(current_avg,!pitStopAppendix==''))+pitStopAppendix;
+            avgDisplay.innerHTML=formatSpeed(wpmOrCpm(current_avg,context==1))+avgAppendix[context];
 		}
 	});
 }
 
 // Get, format and display latest race and point counts
-function addCommas(dataRowClass)
+function addCommas()
 {
-    racesDisplay.innerText = formatInteger(document.querySelector("."+dataRowClass+" > td:nth-child(4)").innerText);
-    pointsDisplay.innerText = formatInteger(document.querySelector("."+dataRowClass+" > td:nth-child(5)").innerText);
+    racesDisplay.innerText = formatInteger(document.querySelector("."+dataRowClass[context]+" > td:nth-child(4)").innerText);
+    pointsDisplay.innerText = formatInteger(document.querySelector("."+dataRowClass[context]+" > td:nth-child(5)").innerText);
 }
-
-
-// Delaying the code's execution, to give the page time to load
-window.addEventListener('load', function()
-{
-    setTimeout(function(){
-        let originalAvgList = document.getElementsByClassName('mainUserInfoBoxWpm');
-
-        if(originalAvgList.length==0) //Pit Stop: using the original average boxes to display data. No need for new boxes, or to refresh them later.
-        {
-            if(document.getElementsByClassName('mainUserName')[0].innerText=="You are not signed in")
-                return;
-
-            avgDisplay = document.querySelector(".dataRow > td:nth-child(3)");
-            exactAvg('mainUserName',' <a href="http://wikipedia.org/wiki/Wpm" target="_blank">WPM</a>');
-
-            racesDisplay = document.querySelector(".dataRow > td:nth-child(4)");
-            pointsDisplay = document.querySelector(".dataRow > td:nth-child(5)");
-            addCommas('dataRow');
-        }
-        else //Main page: using intervals to refresh values. Creating new boxes to display them, and hiding the original ones.
-        {
-            let originalAvg = document.getElementsByClassName('mainUserInfoBoxWpm')[0];
-            originalAvg.style.display = 'none';
-
-            avgDisplay = document.createElement('div');
-            avgDisplay.style.paddingRight = '4px';
-            originalAvg.parentNode.appendChild(avgDisplay);
-
-            exactAvg('userNameLabel'); //Initial refresh
-            setInterval(function(){exactAvg('userNameLabel')},10000); // Refresh avg regularly in case player uses multiple tabs (10-second interval for data.typeracer's sake)
-
-            let dataRow = document.getElementsByClassName('datarow')[0];
-
-            racesDisplay = document.createElement('td');
-            dataRow.appendChild(racesDisplay);
-
-            pointsDisplay = document.createElement('td');
-            pointsDisplay.title = "number of words typed multiplied by typing speed in words-per-second";
-            pointsDisplay.style = "cursor: help;";
-            dataRow.appendChild(pointsDisplay);
-
-            document.querySelector(".datarow > td:nth-child(4)").style.display = "none";
-            document.querySelector(".datarow > td:nth-child(5)").style.display = "none";
-
-            setInterval(function(){addCommas('datarow')},100);
-        }
-    },2000);
-}, false);
 
 // Refresh average on race end
 function detectRaceEnding() {
@@ -157,9 +126,77 @@ function detectRaceEnding() {
     if ((gameStatus.startsWith('You finished') || gameStatus.startsWith('The race has ended'))&&isRacing)
     {
         isRacing=false;
-        exactAvg('userNameLabel');
+        exactAvg();
     }
     else if(gameStatus=='Go!'&&!isRacing)
         isRacing=true;
 }
-setInterval(detectRaceEnding,100);
+
+// Keep the "playerName" value up to date & refresh average when player logs in or out
+function detectLogActivity()
+{
+    let previousName=playerName;
+    playerName = getUsernameFromDisplayName(document.getElementsByClassName(displayNameClass[context])[0].innerText);
+    if(previousName=='')
+        return;
+    if(previousName!=playerName)
+    {
+        exactAvg();
+    }
+}
+
+function initiate()
+{
+	context=checkContext();
+
+	detectLogActivity();
+	setInterval(detectLogActivity,100);
+
+	if(context==0) //Main page: using intervals to refresh values. Creating new boxes to display them, and hiding the original ones.
+	{
+		let originalAvg = document.getElementsByClassName('mainUserInfoBoxWpm')[0];
+		originalAvg.style.display = 'none';
+
+		avgDisplay = document.createElement('div');
+		avgDisplay.style.paddingRight = '4px';
+		originalAvg.parentNode.appendChild(avgDisplay);
+
+		exactAvg(); //Initial refresh
+		setInterval(exactAvg,10000); // Refresh avg regularly in case player uses multiple tabs (10-second interval for data.typeracer's sake)
+
+		let dataRow = document.getElementsByClassName('datarow')[0];
+
+		racesDisplay = document.createElement('td');
+		dataRow.appendChild(racesDisplay);
+
+		pointsDisplay = document.createElement('td');
+		pointsDisplay.title = "number of words typed multiplied by typing speed in words-per-second";
+		pointsDisplay.style = "cursor: help;";
+		dataRow.appendChild(pointsDisplay);
+
+		document.querySelector(".datarow > td:nth-child(4)").style.display = "none";
+		document.querySelector(".datarow > td:nth-child(5)").style.display = "none";
+
+		setInterval(addCommas,100);
+	}
+	else //Pit Stop: using the original average boxes to display data. No need for new boxes, or to refresh them later.
+	{
+		if(document.getElementsByClassName('mainUserName')[0].innerText=="You are not signed in")
+			return;
+
+		avgDisplay = document.querySelector(".dataRow > td:nth-child(3)");
+		exactAvg();
+
+		racesDisplay = document.querySelector(".dataRow > td:nth-child(4)");
+		pointsDisplay = document.querySelector(".dataRow > td:nth-child(5)");
+		addCommas();
+	}
+}
+
+// Delaying the code's execution, to give the page time to load
+window.addEventListener('load', function()
+{
+    setTimeout(function(){
+        initiate();
+    },2000);
+}, false);
