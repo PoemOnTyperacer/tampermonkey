554a4b513457cd86fdf538151613514c2824edc7
PoemOnTyperacer <63360610+PoemOnTyperacer@users.noreply.github.com>
2020-08-19 02:33:36 +0200

    Update d_tr-p.js

--- d_tr-p.js
+++ d_tr-p.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Typeracer: Adjusted speed calculator
 // @namespace    http://tampermonkey.net/
-// @version      1.2.7
+// @version      1.3.0
 // @updateURL    https://raw.githubusercontent.com/PoemOnTyperacer/tampermonkey/master/d_tr-p.js
 // @downloadURL  https://raw.githubusercontent.com/PoemOnTyperacer/tampermonkey/master/d_tr-p.js
 // @description  Adds more values to data.typeracer races details (points/exact speed)
@@ -10,8 +10,7 @@
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
-/*Changelog:
-=================================================================================================================
+/*=========================================CHANGELOG=======================================================
 1.1.0 (04-08-20):   Initial release
 1.2.1 (04-12-20):   Added unlagged and adjusted speed values
                     Forced 2 decimals for speed/3 for adjusted/none for points
@@ -22,7 +21,18 @@
 1.2.6 (04-13-20):   Fixed non-play universes
                     Moved error messages to console
 1.2.7 (04-20-20):   Added Ping value
+1.3.0 (08-19-20):   Added support for quotes starting with a special character, which is escaped in the log (like ")
+                    Added a setting to display the "desslejusted"
+                    (DISCLAIMER: this speed metric does not accurately represent the speed you typed a quote at, but inflates your score.
+                    Therefore, it is not actually in use within the community. I only included it as a meme, and it is disabled by default.)
 =================================================================================================================*/
+
+
+/*=========SETTINGS=============*/
+const SHOW_DESSLEJUSTED = false;
+/*==============================*/
+
+
 
 var race_log = '';
 function consecutiveLogNumbersAfter(k)
@@ -61,15 +71,18 @@ window.addEventListener('load', function() {
     var start_time_ms;
     if(race_log[0]=='\\')
     {
-        if(race_log[1]=='b')
+        if(race_log[1]=='b') //numbers
             start_time_ms=consecutiveLogNumbersAfter(3);
-        else
+        else if(race_log[1]=='u') //unicode characters
             start_time_ms=consecutiveLogNumbersAfter(6);
+        else //escaped characters
+            start_time_ms=consecutiveLogNumbersAfter(2);
     }
     else
         start_time_ms=consecutiveLogNumbersAfter(1);
     var t_start=parseInt(start_time_ms)/12000; // s/12
     var adjusted_speed = ((quote_length-1)/(t_total-t_start)).toFixed(3);
+    var desslejusted = ((quote_length)/(t_total-t_start)).toFixed(3);
 
 	var points = 0;
 	var lagged_speed = 0;
@@ -112,11 +125,14 @@ window.addEventListener('load', function() {
                     if(unlagged_speed < registered_speed)
                         reverse_lag_style=' color:red; font-weight: 1000;';
                     registered_speed = registered_speed.toFixed(2);
-                    unlagged_speed = unlagged_speed.toFixed(2);
+                    unlagged_speed = unlagged_speed.toFixed(8);
                     var points = Math.round(data[i].pts);
                     var ghost_button_html = $('.raceDetails > tbody > tr:nth-child('+univ_index+') > td:nth-child(2) > a')[0].outerHTML.split('<a').join('<a style="position: absolute;left: 100px;"');
 					$('.raceDetails > tbody').append($('<tr><td>Points</td><td>'+points+'</td></tr>'));
-					$('.raceDetails > tbody > tr:nth-child('+univ_index+')')[0].outerHTML = '<br><tr><td>Registered</td><td style="position: relative;'+reverse_lag_style+'"><span>'+registered_speed+' WPM</span>'+ghost_button_html+'</td></tr><tr><td>Unlagged</td><td>'+unlagged_speed+' WPM (ping: '+ping+'ms)</td></tr><tr><td>Adjusted</td><td>'+adjusted_speed+' WPM (start: '+start_time_ms+'ms)</td></tr><br>';
+                    let ds_html='';
+                    if(SHOW_DESSLEJUSTED)
+                        ds_html='<tr><td>Desslejusted</td><td>'+desslejusted+' WPM</td></tr>'
+					$('.raceDetails > tbody > tr:nth-child('+univ_index+')')[0].outerHTML = '<br><tr><td>Registered</td><td style="position: relative;'+reverse_lag_style+'"><span>'+registered_speed+' WPM</span>'+ghost_button_html+'</td></tr><tr><td>Unlagged</td><td>'+unlagged_speed+' WPM (ping: '+ping+'ms)</td></tr><tr><td>Adjusted</td><td>'+adjusted_speed+' WPM (start: '+start_time_ms+'ms)</td></tr>'+ds_html+'<br>';
 				}
 			}
 			});
